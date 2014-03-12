9decd80c337a19e1dad98dfb37e085d1c812bd86
IIIRepublica <pepinoglande@gmail.com>
2014-03-12 13:31:02 +0100

    Posibility of add photos to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
 	validates :name, length: { maximum: 255 }, presence: true
 	has_many :line_items
 	has_many :orders, through: :line_items
+	mount_uploader :photo, ProductPhotoUploader
 end
