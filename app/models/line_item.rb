f80b166a61436864297e79bffd9e77a6db270866
IIIRepublica <pepinoglande@gmail.com>
2014-04-03 15:59:07 +0200

    Added validations at LineItem and Orders models

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  validates :quantity, :unit_price, :product, presence: true
+  validates :quantity, numericality: { only_integer: true }
+	validates :unit_price, numericality: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ActiveRecord::Base
+	ALLOW_STATUS = ['Pendiente', 'Enviado', 'Recibido']
+	ALLOW_PAYMENT_METHODS = ['en efectivo', 'tarjeta', 'paypal']
+
+	validates :customer_name, :customer_last_name, :shipping_address, :shipping_city, :payment_method, :status, presence: true
+	validates :payment_method, inclusion: { in: ALLOW_PAYMENT_METHODS, message: "must be in #{ALLOW_PAYMENT_METHODS.join(', ')}" }
+	validates :status, inclusion: { in: ALLOW_STATUS, message: "must be in #{ALLOW_STATUS.join(', ')}" }
+
 	has_many :line_items
 	has_many :products, through: :line_items
 	accepts_nested_attributes_for :line_items, :allow_destroy => true, 
