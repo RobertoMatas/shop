bd4455c58b6d1b392f36c347be42ad6d4679fe12
IIIRepublica <pepinoglande@gmail.com>
2014-04-03 17:29:27 +0200

    Callbacks for set status in a creation of an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,22 @@ class Order < ActiveRecord::Base
 	ALLOW_STATUS = ['Pendiente', 'Enviado', 'Recibido']
 	ALLOW_PAYMENT_METHODS = ['en efectivo', 'tarjeta', 'paypal']
 
-	validates :customer_name, :customer_last_name, :shipping_address, :shipping_city, :payment_method, :status, presence: true
+	before_create :set_order_status
+
+	validates :customer_name, :customer_last_name, :shipping_address, :shipping_city, :payment_method, presence: true
 	validates :payment_method, inclusion: { in: ALLOW_PAYMENT_METHODS, message: "must be in #{ALLOW_PAYMENT_METHODS.join(', ')}" }
-	validates :status, inclusion: { in: ALLOW_STATUS, message: "must be in #{ALLOW_STATUS.join(', ')}" }
+	validates :status, presence: true, 
+			inclusion: { in: ALLOW_STATUS, message: "must be in #{ALLOW_STATUS.join(', ')}" }, 
+			on: :update
 
 	has_many :line_items
 	has_many :products, through: :line_items
 	accepts_nested_attributes_for :line_items, :allow_destroy => true, 
 		:reject_if => lambda { |li| li[:product_id].blank? or li[:quantity].blank? }
+
+	protected
+		def set_order_status
+			self.status = 'Pendiente'
+		end
 
 end
